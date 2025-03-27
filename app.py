932f86655067f9eba743edec5479697a1fed8a8d
Aydan Thompson <aydan.thompson@bt.com>
2025-03-27 21:53:10 +0000

    Switched to `CliCredentialsStackSynthesizer`

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import aws_cdk as cdk
-from aws_cdk import LegacyStackSynthesizer
+from aws_cdk import CliCredentialsStackSynthesizer
 
 from aws.aws_stack import MyBucketStack
 
 app = cdk.App()
 
+account_id = app.node.try_get_context("account_id")
+lab_role_arn = app.node.try_get_context("lab_role_arn")
+
 MyBucketStack(
     app,
     "MyBucketStack",
-    synthesizer=LegacyStackSynthesizer(),
+    synthesizer=CliCredentialsStackSynthesizer(),
+    lab_role_arn=lab_role_arn,
 )
 
 app.synth()
--- aws/aws_stack.py
+++ aws/aws_stack.py
@@ -1,10 +1,18 @@
-from aws_cdk import Stack
+from aws_cdk import RemovalPolicy, Stack
+from aws_cdk import aws_apigateway as api_gw
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
 
 class MyBucketStack(Stack):
-    def __init__(self, scope: Construct, id: str, **kwargs):
+    def __init__(self, scope: Construct, id: str, lab_role_arn: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        s3.Bucket(self, "TestBucket01", versioned=True)
+        s3.Bucket(
+            self,
+            "TestBucket01",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
