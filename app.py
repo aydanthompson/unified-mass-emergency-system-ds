4e3dd889fdb0aef5ea4bfb3a268c4bf350f0d151
Aydan <aydanthompson20@gmail.com>
2025-03-27 23:49:39 +0000

    Added basic Lambda and SNS

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import aws_cdk as cdk
-from aws_cdk import CliCredentialsStackSynthesizer
+from aws_cdk import App, CliCredentialsStackSynthesizer
 
-from aws.aws_stack import MyBucketStack
+from aws.aws_stack import IoTWeatherAlertStack
 
-app = cdk.App()
+app = App()
 
 account_id = app.node.try_get_context("account_id")
 lab_role_arn = app.node.try_get_context("lab_role_arn")
+sensor_threshold = app.node.try_get_context("sensor_threshold") or 40.0
 
-MyBucketStack(
+IoTWeatherAlertStack(
     app,
-    "MyBucketStack",
-    synthesizer=CliCredentialsStackSynthesizer(),
+    "IoTWeatherAlertStack",
     lab_role_arn=lab_role_arn,
+    sensor_threshold=sensor_threshold,
+    synthesizer=CliCredentialsStackSynthesizer(),
 )
 
 app.synth()
